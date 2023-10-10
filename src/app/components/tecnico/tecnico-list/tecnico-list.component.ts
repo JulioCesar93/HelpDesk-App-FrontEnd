fb0def2428998ca6e7522f93d1b56eb34cec5ae0
julio_m3_@hotmail.com <julio_m3_@hotmail.com>
2023-10-09 22:25:59 -0300

    Componente LOGIN criado

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
   templateUrl: './tecnico-list.component.html',
   styleUrls: ['./tecnico-list.component.css']
 })
-export class TecnicoListComponent {
+export class TecnicoListComponent implements OnInit{
+  
+  ELEMENT_DATA: Tecnico [] = [
+    {
+      id: 1,
+      nome: 'Julio Cesar',
+      cpf: '111.200.301-51',
+      email: 'julio@mail.com',
+      senha: '8888',
+      perfis: ['0'],
+      dataCriacao: '02/11/2023'
 
+    }
+  ]
+
+  displayedColumns = ['id', 'name', 'cpf', 'email', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor () {}
+
+  ngOnInit(): void { }
 }
+
+
+
---
+++ src/app/models/tecnico.ts
@@ -0,0 +1,9 @@
+export interface Tecnico {
+    id?:    any; // String ou Int
+    nome:   string;
+    cpf:    string;
+    email:  string;
+    senha:  string;
+    perfis: string[];
+    dataCriacao: any;
+}
